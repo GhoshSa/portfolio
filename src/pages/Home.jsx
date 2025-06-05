1a640b71f38d8f74769dc9e19072ad0cd25cc7db
Sayantan Ghosh <ghoshsayantan710@gmail.com>
2025-06-05 20:44:05 +0530

    Light and Dark background themes implemented

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import ThemeToggle from "../components/ThemeToggle"
+import StarBackground from "../components/StarBackground"
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle/>
+      <StarBackground/>
+    </div>
+  )
+}
+
+export default Home
